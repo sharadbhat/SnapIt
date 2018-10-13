d42a62c3cdc9ebe60acee9700f44c6220e150970
Sharad Bhat <sharad.mbhat@gmail.com>
2018-10-13 20:35:04 +0530

    Added functions for search, getting user images and random images

--- database.py
+++ database.py
@@ -7,6 +7,20 @@ class Database:
         self.db = pymysql.connect(host='localhost', user='root', passwd=password, db='unsplash')
         self.cur = self.db.cursor()
 
+    def get_random_images(self, n):
+        self.cur.execute(f'SELECT id FROM images ORDER BY RAND() LIMIT {n}')
+        images = []
+        for i in self.cur.fetchall():
+            images.append(i[0])
+        return images
+
+    def get_random_images_and_title(self, n):
+        self.cur.execute(f'SELECT id, title FROM images ORDER BY RAND() LIMIT {n}')
+        images = {}
+        for i in self.cur.fetchall():
+            images[i[0]] = i[1]
+        return images
+
     def add_user(self, username, plain_password):
         hash = generate_password_hash(plain_password)
         try:
@@ -28,7 +42,22 @@ class Database:
 
     def add_image(self, image_id, username, title, tags):
         try:
+            tags = " ".join(tags)
             self.cur.execute(f'INSERT INTO images VALUES("{image_id}", "{title}", "{username}", 0, "{tags}")')
             self.db.commit()
         except:
             self.db.rollback()
+
+    def search(self, query):
+        self.cur.execute(f'SELECT id, title FROM images WHERE tags LIKE "%{query}%"')
+        images = {}
+        for i in self.cur.fetchall():
+            images[i[0]] = i[1]
+        return images
+
+    def get_user_images(self, username):
+        self.cur.execute(f'SELECT id, title FROM images WHERE uploader = "{username}"')
+        images = {}
+        for i in self.cur.fetchall():
+            images[i[0]] = i[1]
+        return images
